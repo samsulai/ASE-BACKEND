b07a830513eea13e423d15d5fdc2e704b6abdf0a
MAC <mac@MACs-MacBook-Pro.local>
2023-11-18 12:23:05 +0100

    ädded

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "public/assets")));
 
-app.use(timeout('30s')); 
+app.use(timeout('60s')); 
 
 /* ROUTES */
 app.use("/posts", postRoutes);
